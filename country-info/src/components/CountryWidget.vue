f4c8e1208ca208bcf376db40dd7364927028f168
filip <filip@smartiot.cloud>
2024-10-22 14:37:58 +0300

    added CountryWidget comp

---
+++ country-info/src/components/CountryWidget.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="country in countries" :key="country.name" class="widget-item">
+      <div><strong>{{ country.name }}</strong></div>
+      <div>Next Holiday: {{ country.holiday }}</div>
+      <div>Date: {{ country.date }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'CountryWidget',
+  props: {
+    countries: {
+      type: Array as PropType<{ name: string; holiday: string; date: string }[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.widget-item {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
