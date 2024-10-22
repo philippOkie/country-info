4c36cccf7f197d7312bbc6d127c494c14c19c53e
filip <filip@smartiot.cloud>
2024-10-22 16:29:45 +0300

    added routing

--- country-info/src/App.vue
+++ country-info/src/App.vue
@@ -1,80 +1,11 @@
 <template>
   <div id="app">
-    <div class="container">
-      <div>
-        <input type="text" placeholder="Search Input" v-model="searchTerm" />
-        <h3>Countries List</h3>
-       <CountryList :countries="filteredCountries" />
-      </div>
-
-      <div class="random-widget">
-        <h3>Random Countries Widget</h3>
-        <CountryWidget :countries="filteredCountries" />
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
-<script lang="ts">
-import { ref, computed, onMounted, defineComponent } from 'vue';
-
-import CountryList from './components/CountryList.vue';
-import CountryWidget from './components/CountryWidget.vue';
-
-const baseUrl = 'https://date.nager.at/api/v3'
-
-export default defineComponent({
+<script>
+export default {
   name: 'App',
-  components: { CountryList, CountryWidget },
-  setup() {
-    const countries = ref<{ name: string; code: string }[]>([]);
-    const searchTerm = ref('');
-
-    const fetchCountries = async () => {
-      try {
-        const response = await fetch(`${baseUrl}/AvailableCountries`);
-        const data = await response.json();
-
-        countries.value = data.map((country: any) => ({
-          name: country.name,
-          code: country.countryCode,
-        }));
-      } catch (error) {
-        console.error('Error fetching countries:', error);
-      }
-    };
-
-    const filteredCountries = computed(() => {
-      return countries.value.filter((country) =>
-        country.name.toLowerCase().includes(searchTerm.value.toLowerCase())
-      );
-    });
-
-    onMounted(fetchCountries);
-
-    return {
-      countries,
-      searchTerm,
-      filteredCountries,
-    };
-  },
-});
+};
 </script>
-
-<style scoped>
-  .container {
-    display: flex;
-    gap: 9px;
-  } 
-
-  .random-widget {
-    overflow: scroll;
-    width: 100%;
-    height: 100vh;
-  }
-
-  input {
-    padding: 8px;
-    margin-bottom: 10px;
-  }
-</style>
--- country-info/src/router/index.ts
+++ country-info/src/router/index.ts
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../components/CountryWidget.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CountryPage from '../views/CountryPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../components/CountryList.vue')
-    }
-  ]
-})
+      path: '/country/:code',
+      name: 'country',
+      component: CountryPage,
+      props: true,
+    },
+  ],
+});
 
-export default router
+export default router;
