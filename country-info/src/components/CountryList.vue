f93f89c2ed4b9dca630fde79d8c39c1d9de04c38
filip <filip@smartiot.cloud>
2024-10-22 14:38:51 +0300

    added country list

---
+++ country-info/src/components/CountryList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="country in countries" :key="country.name" class="country-item">
+      {{ country.name }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'CountryList',
+  props: {
+    countries: {
+      type: Array as PropType<{ name: string; holiday: string; date: string }[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.country-item {
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+</style>
